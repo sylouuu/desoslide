876c0c621253f3b678802c9be440f800bdd754d5
sylouuu <sylouuu@gmail.com>
2013-04-06 20:14:45 +0200

    added data-href feature

--- js/jquery.desoslide.js
+++ js/jquery.desoslide.js
@@ -80,6 +80,7 @@ This source code is under the MIT License
 					src = $('a', $thumbs).eq(currentImg).attr('href');
 					alt = $('img', $thumbs).eq(currentImg).attr('alt');
 					captionInfo = $('img', $thumbs).eq(currentImg).data('caption');
+					href = $('img', $thumbs).eq(currentImg).data('href');
 					// console.log($('img', $(p.mainImage)).selector +' : '+ captionInfo);
 					
 					// captions checks
@@ -99,6 +100,7 @@ This source code is under the MIT License
 						'data-caption'	: captionInfo
 					});
 					
+					// the wrapper tag
 					$wrapper = $('<div>', {
 						'class': 'desoSlide_wrapper'
 					});
@@ -119,13 +121,17 @@ This source code is under the MIT License
 						break;
 					}
 
+					
 					if(!p.disableCaption) {
 						$('img', $(p.mainImage)).one('load', function() {
-							app.calculateCaptionPosition(captionInfo);
+							app.calculateCaptionPosition(captionInfo, href);
 						}).each(function() {
 							if(this.complete) $(this).load();
 						});
+					} else {
+						app.addLink(href);
 					}
+
 				} else {
 					if(thumbsCount == 0) {
 						app.displayError('You must have at least 1 thumbnail.');
@@ -156,7 +162,7 @@ This source code is under the MIT License
 						$overlay.animate({ opacity: 0 });
 						
 						// call the displayer
-						app.displayImg($this.attr('href'), $('img', $this).attr('alt'), $('img', $this).data('caption'));
+						app.displayImg($this.attr('href'), $('img', $this).attr('alt'), $('img', $this).data('caption'), $('img', $this).data('href'));
 						
 						// set the current image index
 						currentImg = $this.parent('li').index();
@@ -196,7 +202,7 @@ This source code is under the MIT License
 				// new caption position when resizing
 				$(window).resize(function() {
 					if(!p.disableCaption && returnValue.selector == $thumbsContainer.selector) {
-						app.calculateCaptionPosition(captionInfo);
+						app.calculateCaptionPosition(captionInfo, href);
 					}
 				});
 				
@@ -211,7 +217,7 @@ This source code is under the MIT License
 			// *****************
 			
 			// adjust the caption position
-			calculateCaptionPosition: function(info) {
+			calculateCaptionPosition: function(info, href) {
 				var width = 0;
 				var height = 0;
 				
@@ -261,17 +267,19 @@ This source code is under the MIT License
 						opacity: 0.7
 					});
 				}
+				
+				app.addLink(href);
 			
 			},
 			
 			// displaying the new image
-			displayImg: function(href, alt, info) {
+			displayImg: function(src, alt, info, href) {
 				var self = this;
 				$('img', $(p.mainImage)).stop().animate({
 					opacity: 0
 				}, 400, function() {
 					$(this).attr({
-						'src': href,
+						'src': src,
 						'alt': alt,
 						'data-caption': info
 					}).load(function() {
@@ -279,11 +287,37 @@ This source code is under the MIT License
 							opacity: 1
 						}, 400, function() {
 							if(!p.disableCaption) {
-								app.calculateCaptionPosition(info);
-							}					
+								app.calculateCaptionPosition(info, href);
+							}	else {
+								app.addLink(href);
+							}
 						});
 					});
 				})
+			},
+			
+			addLink: function(href) {
+				var aExists = ($('a', $(p.mainImage)).length > 0) ? true : false;
+				var hrefExists = (typeof href !== 'undefined' && href != '') ? true : false;
+
+				// the <a> tag
+				var $a = $('<a>', {
+					'href'			: href,
+					'target'		: '_blank'
+				});
+				
+				if(aExists && hrefExists) {
+					// update the href
+					$('img', $(p.mainImage)).parent('a').attr('href', href);
+				} else if(aExists && !hrefExists) {
+					 // replace the <a> tag with this content
+					var content = $('a', $(p.mainImage)).contents();
+					$('a', $(p.mainImage)).replaceWith(content);
+				} else if(!aExists && hrefExists) {
+					// adding the <a> tag
+					var content = $('.desoSlide_wrapper', $(p.mainImage)).contents();
+					$a.appendTo($('.desoSlide_wrapper', $(p.mainImage))).html(content);
+				}				
 			},
 			
 			// displaying warning message in the console
