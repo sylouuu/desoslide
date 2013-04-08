9cd191b767135d37e0f564a9fe8efd9cc68fe3c3
sylouuu <sylouuu@gmail.com>
2013-04-08 20:00:05 +0200

    sprites

--- js/jquery.desoslide.js
+++ js/jquery.desoslide.js
@@ -304,17 +304,17 @@ This source code is under the MIT License
 			
 			// add controls
 			addControls: function() {		
-				$('.desoSlide_controls', $(p.mainImage)).remove();
+				$('.desoSlide_controls-wrapper', $(p.mainImage)).remove();
 				
 				// controls buttons			
-				var $prev	= '<a href="#prev"><span class="prev">prev</span></a>';
-				var $pause	= '<a href="#pause"><span class="pause">pause</span></a>';
-				var $play	= '<a href="#play"><span class="play">play</span></a>';
-				var $next	= '<a href="#next"><span class="next">next</span></a>';
+				var $prev	= '<a href="#prev"><span class="desoSlide_controls prev">test</span></a>';
+				var $pause	= '<a href="#pause"><span class="desoSlide_controls pause"></span></a>';
+				var $play	= '<a href="#play"><span class="desoSlide_controls play"></span></a>';
+				var $next	= '<a href="#next"><span class="desoSlide_controls next"></span></a>';
 				
 				// the wrapper
 				var $controls = $('<div>', {
-					'class': 'desoSlide_controls'
+					'class': 'desoSlide_controls-wrapper'
 				}).append($prev + $pause + $play + $next);
 				
 				// dynamic positioning
