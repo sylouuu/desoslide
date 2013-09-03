e1af8f665267ff375eb86807402daf0daa96f6dc
sylouuu <s.valienne@yahoo.fr>
2013-09-03 11:18:28 +0200

    doc: affix test 2

--- assets/js/doc/documentation.js
+++ assets/js/doc/documentation.js
@@ -45,4 +45,18 @@ $(function() {
 		}
 	});
 
+	/* Affix */
+	var $sideBar = $('.sidebar')
+
+	$sideBar.affix({
+		offset: {
+			top: function () {
+				var offsetTop      = $sideBar.offset().top;
+				var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10);
+
+				return (this.top = offsetTop - sideBarMargin);
+			}
+		}
+	});
+
 });
