293337d1b8bc74a60caae6e63c4a2984b40022c2
Syl <s.valienne@yahoo.fr>
2014-05-03 19:13:17 +0200

    home wow test 1

---
+++ doc/assets/js/app/home.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    $('a.back-to-top').on('click', function(e) {
+        e.preventDefault();
+
+        $('html, body').stop().animate({
+            scrollTop: $('html, body').offset().top
+        }, 1000);
+    });
+
+    new WOW().init();
+});
