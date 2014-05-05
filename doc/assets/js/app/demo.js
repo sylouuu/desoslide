3d6045f1662d61c2a1ba4b03f32208c003267357
Syl <sylvain@appturbo.it>
2014-05-05 10:52:30 +0200

    updated home & caption position

--- doc/assets/js/app/demo.js
+++ doc/assets/js/app/demo.js
@@ -12,18 +12,33 @@ $(function() {
         }, 1000);
     });
 
+    // Syntax highlighting
+    // ----------------------------------------------------------------------------------------------------------
+
+    var syntaxHighlight = function() {
+        $('pre code').each(function(i, e) {
+            hljs.highlightBlock(e);
+        });
+    };
+
+    syntaxHighlight();
+
     // Demo 1
     // ----------------------------------------------------------------------------------------------------------
 
-    $('#demo1_image').desoSlide({
+    var $demo1_image = $('#demo1_image'), $effect_provider = $('#effect_provider'), $effect_name = $('#effect_name');
+
+    $demo1_image.desoSlide({
         thumbs: $('#demo1_thumbs_list1, #demo1_thumbs_list2').find('li > a')
     });
 
+    // Available effects by provider
     var effects = {
         animate: ['fade', 'sideFade', 'sideFadeBig', 'flip', 'light', 'roll', 'rotate'],
         magic: ['foolish', 'swash', 'tin', 'puff', 'twister']
     };
 
+    // Change provider
     var changeProvider = function(provider) {
         var tmp = '';
 
@@ -31,38 +46,48 @@ $(function() {
             tmp += '<option value="'+ item +'">'+ item +'</option>';
         });
 
-        $('#effect_name').html(tmp);
+        $effect_name.html(tmp);
     };
 
+    // Set effect
     var setEffect = function(provider, effect) {
-        $('#demo1_image').desoSlide('pause');
+        // Pausing
+        $demo1_image.desoSlide('pause');
 
-        $('#demo1_image').desoSlide('setEffect', { provider: $('#effect_provider').val(), name: $('#effect_name').val() });
+        // Applying asked effect
+        $demo1_image.desoSlide('setEffect', { provider: $effect_provider.val(), name: $effect_name.val() });
 
-        $('#demo1_image').desoSlide('play');
+        // Playing
+        $demo1_image.desoSlide('play');
 
         var code = "$('#demo1_image').desoSlide({\n";
         code += "    thumbs: $('#demo1_thumbs_list1, #demo1_thumbs_list2').find('li > a'),\n";
         code += "    effect: {\n";
-        code += "        provider: '"+ $('#effect_provider').val() +"',\n";
-        code += "        name: '"+ $('#effect_name').val() +"'\n";
+        code += "        provider: '"+ $effect_provider.val() +"',\n";
+        code += "        name: '"+ $effect_name.val() +"'\n";
         code += "    }\n";
         code += "});";
 
-        $('#code1').html(code);
+        $('#html_code1').html('&lt;link rel="stylesheet" href="path/to/'+ $effect_provider.val() +'.css"&gt;');
+        $('#js_code1').html(code);
+
+        syntaxHighlight();
     };
 
 
-    $('#effect_provider').on('change', function() {
+    // On change effect provider
+    $effect_provider.on('change', function() {
         changeProvider($(this).val());
 
         setEffect();
     });
 
-    $('#effect_name').on('change', function() {
+    // On change effect name
+    $effect_name.on('change', function() {
         setEffect();
     });
 
+    // Default loaded provider
     changeProvider('animate');
 
     // Demo 2
@@ -109,10 +134,6 @@ $(function() {
             name: 'twister'
         },
         overlay: 'none'
-    });
-
-    $('pre code').each(function(i, e) {
-        hljs.highlightBlock(e);
     });
 
 });
